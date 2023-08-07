1442bcba3dd4edbeb076927efa4d207777d72264
DESKTOP-4QLEUQH\wagr <wargr6177@gmail.com>
2023-08-07 17:57:36 +0900

    serg

--- project/golf.sql
+++ project/golf.sql
@@ -311,21 +311,35 @@ order by 총투표건수 desc;
 
 
 --ALTER TABLE tbl_vote_202005
---RENAME COLUMN V_CONFIRM TO 유권자확인;
+--RENAME COLUMN 유권자확인 TO V_CONFIRM;
 
 -- 홈쇼핑
 
 
-SELECT * FROM MEMBER_TBL_02;
+SELECT * FROM MEMBER_TBL_02 order by custno desc;
 
 ALTER TABLE MEMBER_TBL_02
 RENAME COLUMN 도시코드 TO city;
 
 INSERT INTO MEMBER_TBL_02 VALUES('100006','차공단','010-1111-7777','제주도 제주시 감나무골','20151211','C','60');
+INSERT INTO MEMBER_TBL_02 VALUES('100005','진평화','010-1111-6666','제주도 제주시 외나무골','20151225','B','60');
+INSERT INTO MEMBER_TBL_02 VALUES('100004','최사랑','010-1111-5555','울릉군 울릉읍 독도2리','20151113','A','30');
+INSERT INTO MEMBER_TBL_02 VALUES('100003','장믿음','010-1111-4444','울릉군 울릉읍 독도1리','20151001','B','30');
+INSERT INTO MEMBER_TBL_02 VALUES('100002','이축복','010-1111-3333','서울 동대문구 휘경2동','20151206','B','01');
+INSERT INTO MEMBER_TBL_02 VALUES('100001','김행복','010-1111-2222','서울 동대문구 휘경1동','20151202','A','01');
 
 SELECT * FROM MONEY_TBL_02;
 
-INSERT INTO MONEY_TBL_02 VALUES('100004','20160010','3000','1','3000','A007','20160106');
+INSERT INTO MONEY_TBL_02 VALUES('100001','20160001','500','5','2500','A001','20160101');
+INSERT INTO MONEY_TBL_02 VALUES('100002','20160002','1000','4','4000','A002','20160101');
+INSERT INTO MONEY_TBL_02 VALUES('100003','20160003','500','3','1500','A008','20160101');
+INSERT INTO MONEY_TBL_02 VALUES('100004','20160004','2000','1','2000','A004','20160102');
+INSERT INTO MONEY_TBL_02 VALUES('100005','20160005','500','1','500','A001','20160103');
+INSERT INTO MONEY_TBL_02 VALUES('100006','20160006','1500','2','3000','A003','20160103');
+INSERT INTO MONEY_TBL_02 VALUES('100007','20160007','500','2','1000','A001','20160104');
+INSERT INTO MONEY_TBL_02 VALUES('100008','20160008','300','1','300','A005','20160104');
+INSERT INTO MONEY_TBL_02 VALUES('100009','20160009','600','1','600','A006','20160104');
+INSERT INTO MONEY_TBL_02 VALUES('100010','20160010','3000','1','3000','A007','20160106');
 
 
 --회원목록조회/수정
@@ -354,18 +368,29 @@ order by 매출 desc;
 
 
 select a.custno as 회원번호, a. custname as 회원성명,
-case when substr(grade,1,1) = 'A' then 'VIP'
-         when substr(grade,1,1) = 'B' then '일반'
-         when substr(grade,1,1) = 'C' then '직원'
+case when substr(a.grade,1,1) = 'A' then 'VIP'
+         when substr(a.grade,1,1) = 'B' then '일반'
+         when substr(a.GRADE,1,1) = 'C' then '직원'
 end as 고객등급,
-count(d.price) as 매출
+sum(d.price) as 매출
 from MEMBER_TBL_02 a, MONEY_TBL_02 d
 where a.custno = d. custno
-
-group by d.price, d. price
-
+group by a.custno, a. custname, d.price, a.GRADE,a.custno
 order by 매출 desc;
 
+
+
+--회원 매출 조회
+select a.custno as 회원번호, a. custname as 회원성명,
+case when substr(a.grade,1,1) = 'A' then 'VIP'
+         when substr(a.grade,1,1) = 'B' then '일반'
+         when substr(a.GRADE,1,1) = 'C' then '직원'
+end as 고객등급,
+sum(d.price) as 매출
+from MEMBER_TBL_02 a, MONEY_TBL_02 d
+where a.custno = d. custno
+group by a.custno, a.custname, a.GRADE
+order by 매출 desc;
 
 
 
