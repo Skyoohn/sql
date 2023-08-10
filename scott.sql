c32f79f74ec7eaa0a403520dba354fb755b28f79
DESKTOP-4QLEUQH\wagr <wargr6177@gmail.com>
2023-08-10 17:22:43 +0900

    esrg

--- scott.sql
+++ scott.sql
@@ -888,3 +888,38 @@ WHERE ENAME LIKE '%T%');
 
 
 select * from mvc_board;
+
+
+
+begin
+  for i in 1..1000 loop
+   insert into mvc_board (bId, bName, bTitle, bContent, bHit, bGroup, bStep, bIndent) 
+   values (mvc_board_seq.nextval, 'test' , '테스트', '테스트', 0, mvc_board_seq.currval, 0, 0);
+  end loop;
+commit;
+end;
+
+
+commit;
+
+select * from mvc_board where bid >= 1 and bid <= 10;
+--rownum : 조회후 순번을 매길때 사용
+
+select rownum, a.* from emp a;
+
+select rownum rn, m.* from mvc_board m where rownum <=10;
+
+
+select rownum rn, m.* from mvc_board m where rownum >0 and rownum <= 20;
+
+
+
+        SELECT * FROM (
+              SELECT ROWNUM AS RNUM, A.* FROM (
+                       SELECT
+                             *
+                         FROM
+                             mvc_board 
+                         order by bGroup desc, bStep asc   
+             ) A WHERE ROWNUM <= 20
+         ) WHERE RNUM > 10;
