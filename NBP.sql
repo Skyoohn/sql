e2720332ae08fa03dfa8ca18936f7a246f16199f
DESKTOP-4QLEUQH\wagr <wargr6177@gmail.com>
2023-10-17 12:44:10 +0900

    1

--- NBP.sql
+++ NBP.sql
@@ -7,5 +7,10 @@ ntitle varchar2(100) not null,  -- 게시물 제목
 ncontent varchar2(500),         -- 게시물 내용
 ndate date default sysdate,     -- 게시물 작성 일자
 nhit number(4) default 0,       -- 조회수
-nselector number(1) not null --게시글구분 1:공지 2:qna 3:리뷰
+nselector number(1) not null --게시글구분 1:단어장 2:공지 3:일반 게시판
 );
+
+CREATE SEQUENCE nbp_board_seq
+START WITH 1
+INCREMENT BY 1;
+
