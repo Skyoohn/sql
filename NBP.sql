c08f278b1227bee897be4e840ada72d102d16a2c
DESKTOP-4QLEUQH\wagr <wargr6177@gmail.com>
2023-10-18 09:36:47 +0900

    1

--- NBP.sql
+++ NBP.sql
@@ -1,16 +1,25 @@
 select * from nbp_board;
 
 create table nbp_board (
-nid number(6) primary key,      -- 게시물 식별자
-nname varchar2(20) not null,    -- 작성자 이름
 ntitle varchar2(100) not null,  -- 게시물 제목
-ncontent varchar2(500),         -- 게시물 내용
-ndate date default sysdate,     -- 게시물 작성 일자
-nhit number(4) default 0,       -- 조회수
-nselector number(1) not null --게시글구분 1:단어장 2:공지 3:일반 게시판
+ncontent varchar2(500)  -- 게시물 내용
 );
 
 CREATE SEQUENCE nbp_board_seq
 START WITH 1
 INCREMENT BY 1;
 
+
+create table nbp_user_board (
+uid number(6) primary key,      -- 게시물 식별자
+uname varchar2(20) not null,    -- 작성자 이름
+utitle varchar2(100) not null,  -- 게시물 제목
+ucontent varchar2(500),         -- 게시물 내용
+udate date default sysdate,     -- 게시물 작성 일자
+uhit number(4) default 0,       -- 조회수
+uselector number(1) not null --1:공지 2:일반 게시판
+);
+
+CREATE SEQUENCE nbp_user_board_seq
+START WITH 1
+INCREMENT BY 1;
